c9bcc7d21d2e6cd01af7ac1a6d530f9af127c513
UshurFatima <ushurfatima@gmail.com>
2024-10-13 21:51:44 +0600

    validation

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from posts.models import Post, Category, SearchWord
+from rest_framework.exceptions import ValidationError
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -31,3 +32,34 @@ class PostSerializer(serializers.ModelSerializer):
 
     def get_category_name(self, post):
         return post.category.name if post.category else None
+
+
+class PostValidateSerializer(serializers.Serializer):
+    title = serializers.CharField(min_length=5, max_length=255)
+    text = serializers.CharField(required=False)
+    is_active = serializers.BooleanField()
+    view_count = serializers.IntegerField(min_value=0, max_value=100)
+    category_id = serializers.IntegerField(min_value=1)
+    search_words = serializers.ListField(child=serializers.IntegerField(min_value=1))
+
+    def validate_category_id(self, category_id):
+        try:
+            Category.objects.get(id=category_id)
+        except Category.DoesNotExist:
+            raise ValidationError('Category does not exist!')
+        return category_id
+
+    def validate_search_words(self, search_words):
+        search_words_db = SearchWord.objects.filter(id__in=search_words)
+        if len(search_words_db) != len(search_words):
+            raise ValidationError('Search word does not exist')
+        return search_words
+
+    # def validate(self, attrs):
+    #     category_id = attrs.get('category_id')
+    #     try:
+    #         Category.objects.get(id=category_id)
+    #     except Category.DoesNotExist:
+    #         raise ValidationError('Category does not exist!')
+    #
+    #     return attrs
--- posts/views.py
+++ posts/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 # ответ на запрос: данные (список/JSON) статус запроса (100, 200, 300, 400, 500)
 from posts.models import Post
-from posts.serializers import PostSerializer
+from posts.serializers import PostSerializer, PostValidateSerializer
 from rest_framework import status
 
 
@@ -15,6 +15,7 @@ def post_list_create_api_view(request):
     if request.method == 'GET':
         print(request.query_params)
         search = request.query_params.get('search', '')
+
         # step 1: Collect data(posts) -> Queryset
         #                    один объект                несколько объектов
         posts = (Post.objects.select_related('category')
@@ -34,14 +35,22 @@ def post_list_create_api_view(request):
 
         # step 3: Return response as JSON
         return Response(data=data)
+
     elif request.method == 'POST':
+        # step 0: Validation of data (existing, data type, extra)
+        serializer = PostValidateSerializer(data=request.data)
+        # serializer.is_valid(raise_exception=True)
+        if not serializer.is_valid():
+            return Response(status=status.HTTP_406_NOT_ACCEPTABLE,
+                            data=serializer.errors)
+
         # step 1: Receive data from request body
-        title = request.data.get('title')
-        text = request.data.get('text')
-        is_active = request.data.get('is_active')
-        view_count = request.data.get('view_count')
-        category_id = request.data.get('category_id')
-        search_words = request.data.get('search_words')
+        title = serializer.validated_data.get('title')
+        text = serializer.validated_data.get('text')
+        is_active = serializer.validated_data.get('is_active')
+        view_count = serializer.validated_data.get('view_count')
+        category_id = serializer.validated_data.get('category_id')
+        search_words = serializer.validated_data.get('search_words')
 
         # step 2: Create post by received data
         post = Post.objects.create(
@@ -62,12 +71,19 @@ def post_list_create_api_view(request):
 # информация только об одном объекте
 @api_view(http_method_names=['GET', 'PUT', 'DELETE'])
 def post_detail_api_view(request, id):
+    # step 0: Validation of data (existing, data type, extra)
+    serializer = PostValidateSerializer(data=request.data)
+    if not serializer.is_valid():
+        return Response(status=status.HTTP_406_NOT_ACCEPTABLE,
+                        data=serializer.errors)
+
     # step 1: Collect data(posts) -> Queryset
     try:
         post = Post.objects.get(id=id)
     except Post.DoesNotExist:
         return Response(data={'error': 'Post not found'},
                         status=status.HTTP_404_NOT_FOUND)
+
     if request.method == 'GET':
         # step 2: Reformat posts to list of Dictionaries (JSON)
         data = PostSerializer(instance=post, many=False).data
@@ -76,12 +92,12 @@ def post_detail_api_view(request, id):
         return Response(data=data)
 
     elif request.method == 'PUT':
-        post.title = request.data.get('title')
-        post.text = request.data.get('text')
-        post.is_active = request.data.get('is_active')
-        post.view_count = request.data.get('view_count')
-        post.category_id = request.data.get('category_id')
-        post.search_words.set(request.data.get('search_words'))
+        post.title = serializer.validated_data.get('title')
+        post.text = serializer.validated_data.get('text')
+        post.is_active = serializer.validated_data.get('is_active')
+        post.view_count = serializer.validated_data.get('view_count')
+        post.category_id = serializer.validated_data.get('category_id')
+        post.search_words.set(serializer.validated_data.get('search_words'))
         post.save()
         return Response(data=PostSerializer(post).data,
                         status=status.HTTP_201_CREATED)
